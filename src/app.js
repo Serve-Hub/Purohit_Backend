102266e763c9c77f0b5ce5f61a06232de8b42808
Kuber-Pathak <kuberpathak124@gmail.com>
2024-12-14 20:37:17 +0545

    trying

--- src/app.js
+++ src/app.js
@@ -56,8 +56,8 @@ app.use(cors(corsOptions));
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/admin", adminRouter);
 
-app.use((req, res, next) => {
-  res.status(404).json({ message: "Route not found" });
-});
+// app.use((req, res, next) => {
+//   res.status(404).json({ message: "Route not found" });
+// });
 
 export default app;
