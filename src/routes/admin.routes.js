cf11796f93eac1b100d26aaedbe6f36b36e323f7
Kuber-Pathak <kuberpathak124@gmail.com>
2024-12-15 15:32:51 +0545

    Add JWT authentication middleware and update Swagger documentation for admin routes

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -5,8 +5,11 @@ import {
   deletePuja,
 } from "../controllers/admin.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import verifyJWT from "../middlewares/auth.middleware.js";
 
 const router = Router();
+
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
 router.post("/addPuja", upload.single("pujaImage"), addPuja);
 router.patch("/editPuja/:id", upload.single("pujaImage"), editPuja);
