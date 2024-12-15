5c1e483b5839989f2966a6bfe6b8d4387b7dbda4
Kuber-Pathak <kuberpathak124@gmail.com>
2024-12-15 14:59:51 +0545

    Enable editing of pujas by uncommenting the editPuja route in admin.routes.js

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,7 +1,7 @@
 import Router from "express";
 import {
   addPuja,
-//   editPuja,
+  editPuja,
   deletePuja,
 } from "../controllers/admin.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -9,6 +9,6 @@ import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
 
 router.post("/addPuja", upload.single("pujaImage"), addPuja);
-// router.put("/editPuja/:id", upload.single("pujaImage"), editPuja);
+router.patch("/editPuja/:id", upload.single("pujaImage"), editPuja);
 router.delete("/deletePuja/:id", deletePuja);
 export default router;
